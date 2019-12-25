50b655548d827324baff9dadb0a83eecef171763
Bill Horst <billhorst7@gmail.com>
2019-12-25 15:20:55 -0700

    finished through pseudocode section

---
+++ ls_101_lesson_2/pseudocode.js
@@ -0,0 +1,32 @@
+// a function that returns the sum of two numbers
+
+// START
+// GET first number
+// GET second number
+// PRINT first number + second number
+// END
+
+
+
+// a function that takes an array of strings, and returns a string that is all those strings concatenated together
+
+// START
+// SET concatenatedString = ''
+// LOOP through array
+  // SET concatenatedString = concatenatedString + array element
+// PRINT concatenatedString
+// END
+
+
+
+// a function that takes an array of integers, and returns a new array with every other element
+
+// START
+// SET newArray
+// LOOP through array
+  // IF array element % 2 === 0
+    // PUSH element to newArray
+  // ELSE
+    // MOVE on to next element
+// PRINT newArray
+// END
