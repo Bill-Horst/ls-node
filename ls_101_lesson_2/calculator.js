e6dca21e9d5e7b6827ab0d96f8bad2146b6609ee
Bill Horst <billhorst7@gmail.com>
2019-12-27 21:41:02 -0700

    added sample debug file and added extras to calculator

--- ls_101_lesson_2/calculator.js
+++ ls_101_lesson_2/calculator.js
@@ -1,25 +1,81 @@
 const readline = require('readline-sync');
+const constants = require('./constants');
 
-console.log('Welcome to Calculator!');
-
-console.log('What\'s the first number?');
-let number1 = readline.question();
-
-console.log('What\'s the second number?');
-let number2 = readline.question();
-
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
-
-let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') {
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') {
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') {
-  output = Number(number1) / Number(number2);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
-console.log(`Result: ${output}`);
+function inavlidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+let languageChoice = '';
+while (!['1', '2', '3'].includes(languageChoice)) {
+  prompt(constants.GET_LANGUAGE);
+  languageChoice = readline.question();
+}
+
+let chosenLanguage = setLanguage(languageChoice);
+
+prompt(constants[chosenLanguage].WELCOME);
+
+let continueGame = true;
+while (continueGame) {
+  performOperations();
+  prompt(constants[chosenLanguage].CONTINUE_PROMPT);
+  continueGame = readline.question() === 'y';
+}
+
+function performOperations() {
+  prompt(constants[chosenLanguage].GET_FIRST_NUMBER);
+  let number1 = readline.question();
+  while (inavlidNumber(number1)) {
+    prompt(constants[chosenLanguage].NOT_VALID_NUMBER);
+    number1 = readline.question();
+  }
+
+  prompt(constants[chosenLanguage].GET_SECOND_NUMBER);
+  let number2 = readline.question();
+  while (inavlidNumber(number2)) {
+    prompt(constants[chosenLanguage].NOT_VALID_NUMBER);
+    number2 = readline.question();
+  }
+
+  prompt(constants[chosenLanguage].GET_OPERATION);
+  let operation = readline.question();
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(constants[chosenLanguage].MUST_CHOOSE_1234_MESSAGE);
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`${constants[chosenLanguage].RESULT}: ${output}`);
+}
+
+function setLanguage(lang) {
+  switch (lang) {
+    case '1':
+      return 'ENGLISH';
+    case '2':
+      return 'SPANISH';
+    case '3':
+      return 'JAPANESE';
+    default:
+      return 'INVALID';
+  }
+}
---
+++ ls_101_lesson_2/constants.js
@@ -0,0 +1,33 @@
+module.exports = {
+  GET_LANGUAGE: '1) English, 2) Español, 3)日本語',
+  ENGLISH: {
+    WELCOME: 'Welcome to Calculator!',
+    CONTINUE_PROMPT: 'Would you like to do some more?\nEnter \'y\' to do some more or anything else to quit',
+    NOT_VALID_NUMBER: 'That doesn\'t look like a valid number...',
+    GET_OPERATION: 'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide',
+    GET_FIRST_NUMBER: 'What\'s the first number?',
+    GET_SECOND_NUMBER: 'What\'s the second number?',
+    MUST_CHOOSE_1234_MESSAGE: 'Must choose 1, 2, 3, or 4',
+    RESULT: 'Result'
+  },
+  SPANISH: {
+    WELCOME: '¡Bienvenido a la Calculadora!',
+    CONTINUE_PROMPT: '¿Le gustaría hacer más? Ingrese \'y\' para continuar. Ingrese cualquier otra cosa para salir.',
+    NOT_VALID_NUMBER: 'Eso no parece número válido....',
+    GET_OPERATION: '¿Qué operación quiere realizar?\n1) Añadir 2) Sustraer 3) Multiplicar 4) Dividir',
+    GET_FIRST_NUMBER: '¿Cuál es el primer número?',
+    GET_SECOND_NUMBER: '¿Cuál es el segundo número?',
+    MUST_CHOOSE_1234_MESSAGE: 'Debe elegir entre estos números: 1, 2, 3, or 4',
+    RESULT: 'Resultado'
+  },
+  JAPANESE: {
+    WELCOME: '電卓へようこそ!',
+    CONTINUE_PROMPT: '続きますか？？「y」を入力すれば続行します。 他の入力すれば終了します。',
+    NOT_VALID_NUMBER: 'それは有効な数字のようには見えません。',
+    GET_OPERATION: 'どの操作を選択しますか？\n1) 足し算 2) 引き算 3) 掛け算 4) 割り算',
+    GET_FIRST_NUMBER: '最初の数字は何ですか？',
+    GET_SECOND_NUMBER: '2番目の数字は何ですか？',
+    MUST_CHOOSE_1234_MESSAGE: '１、２、３、４、の中から選んじゃいけません。',
+    RESULT: '結果'
+  }
+};
