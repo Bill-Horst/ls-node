e5912349fe752f70ad78e10b0884db4f05297c79
Bill Horst <billhorst7@gmail.com>
2019-12-28 22:51:54 -0700

    refactored - broke program out into smaller functions, removed eslint disable overrides

--- ls_101_lesson_2/loan.js
+++ ls_101_lesson_2/loan.js
@@ -15,8 +15,79 @@ while (userCalculatesAgain) {
   userCalculatesAgain = rl.question().toLowerCase() === 'y';
 }
 
-
 // FUNCTIONS
+
+function doCalculations() { // orchestration function
+  getLoanAmount();
+  getAPR();
+  getLoanDuration();
+  calculateInterestRateAndPayments();
+  printOutput();
+}
+
+function getLoanAmount() {
+  prompt('What\'s the loan amount? (don\'t include characters like "$")');
+
+  while (true) {
+    loanAmount = rl.question();
+    if (validateNumber(loanAmount, true)) break;
+    else prompt('That\'s not a valid loan amount... Input a number for loan amount.');
+  }
+}
+
+function getAPR() {
+  prompt('What\'s the APR? (don\'t include characters like "%")');
+  prompt('For example, a 5% rate is input as "5", not "5%" or ".05".');
+
+  while (true) {
+    apr = rl.question();
+    if (validateNumber(apr)) break;
+    else prompt('That\'s not a valid apr amount... Input a percentage number for the APR.');
+  }
+}
+
+function getLoanDuration() {
+  prompt('What\'s the loan duration in years? (don\'t include words like "years")');
+  prompt('For example, a one year loan is input as "1". An 18 month loan is input as "1.5".');
+
+  while (true) {
+    loanDuration = rl.question();
+    if (validateNumber(loanDuration, true)) break;
+    else prompt('That\'s not a valid duration... Input a number for duration in years.');
+  }
+}
+
+function calculateInterestRateAndPayments() {
+
+  monthlyInterestRate = calculateMonthlyInterestRate(apr);
+
+  monthlyPayments = calculateMonthlyPayments(
+    loanAmount,
+    monthlyInterestRate,
+    loanDuration * 12
+  );
+}
+
+function printOutput() {
+  console.log(`
+  Loan amount: $${loanAmount}, 
+  APR:         ${apr}%, 
+  Duration:    ${loanDuration} year${Number(loanDuration) === 1 ? '' : 's'},
+  Result:      $${Number(monthlyPayments).toFixed(2)} per month,
+  
+  (After ${loanDuration} year${Number(loanDuration) === 1 ? '' : 's'}, you'll have paid back a total of about $${Number(monthlyPayments * loanDuration * 12).toFixed(2)} and the interest paid will be about $${Number((monthlyPayments * loanDuration * 12) - loanAmount).toFixed()}!)
+  `);
+}
+
+function calculateMonthlyPayments(amount, interest, duration) {
+  return interest === 0
+    ? (amount / duration)
+    : amount * (interest / (1 - Math.pow((1 + interest), (-duration))));
+}
+
+function calculateMonthlyInterestRate(apr) {
+  return apr / 1200;
+}
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -27,56 +98,4 @@ function validateNumber(num, gt0 = false) {
   else if (Number.isNaN(Number(num))) return false; // validate number (not letter(s))
   else if ((!gt0 && num < 0) || (gt0 && num <= 0)) return false; // validate number is positive and greater than 0 if directed
   else return true; // passes all checks
-}
-
-// eslint-disable-next-line
-function calculateMonthlyPayments(p, j, n) {
-  return j === 0 ? (p / n) : p * (j / (1 - Math.pow((1 + j), (-n))));
-}
-
-function calculateMonthlyInterestRate(apr) {
-  return apr / 1200;
-}
-
-// eslint-disable-next-line
-function doCalculations() {
-  prompt('What\'s the loan amount? (don\'t include characters like "$")');
-  while (true) {
-    loanAmount = rl.question();
-    if (validateNumber(loanAmount, true)) break;
-    else prompt('That\'s not a valid loan amount... Input a number for loan amount.');
-  }
-
-  prompt('What\'s the APR? (don\'t include characters like "%")');
-  prompt('For example, a 5% rate is input as "5", not "5%" or ".05".');
-  while (true) {
-    apr = rl.question();
-    if (validateNumber(apr)) break;
-    else prompt('That\'s not a valid apr amount... Input a percentage number for the APR.');
-  }
-
-  prompt('What\'s the loan duration in years? (don\'t include words like "years")');
-  prompt('For example, a one year loan is input as "1". An 18 month loan is input as "1.5".');
-  while (true) {
-    loanDuration = rl.question();
-    if (validateNumber(loanDuration, true)) break;
-    else prompt('That\'s not a valid duration... Input a number for duration in years.');
-  }
-
-  monthlyInterestRate = calculateMonthlyInterestRate(apr);
-
-  monthlyPayments = calculateMonthlyPayments(
-    loanAmount,
-    monthlyInterestRate,
-    loanDuration * 12
-  );
-
-  console.log(`
-  Loan amount: $${loanAmount}, 
-  APR:         ${apr}%, 
-  Duration:    ${loanDuration} year${Number(loanDuration) === 1 ? '' : 's'},
-  Result:      $${Number(monthlyPayments).toFixed(2)} per month,
-  
-  (After ${loanDuration} year${Number(loanDuration) === 1 ? '' : 's'}, you'll have paid back a total of about $${Number(monthlyPayments * loanDuration * 12).toFixed(2)} and the interest paid will be about $${Number((monthlyPayments * loanDuration * 12) - loanAmount).toFixed()}!)
-  `);
 }
